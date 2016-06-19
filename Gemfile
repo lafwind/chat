d688ae92c0ea59d8fc1937a08f4814a526c335f1
Lafwind <lafwind@gmail.com>
2016-06-20 00:44:13 +0800

    config cable.yml, use Redis as an adapter in production and development env

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.0'
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
+gem 'redis', '~> 3.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :chat_rooms, only: [:index, :show, :new, :create]
 
   root to: 'chat_rooms#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount ActionCable.server => '/cable'
 end
