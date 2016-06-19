a9445db57e7c6fdabc479a5ba13d5355755ce507
Lafwind <lafwind@gmail.com>
2016-06-19 21:57:26 +0800

    添加 chat_room controller

---
+++ app/controllers/chat_rooms_controller.rb
@@ -0,0 +1,29 @@
+class ChatRoomsController < ApplicationController
+  def index
+    @chat_rooms = ChatRoom.all
+  end
+
+  def show
+  end
+
+  def new
+    @chat_room = ChatRoom.new
+  end
+
+  def create
+    @chat_room = current_user.chat_rooms.build(chat_room_params)
+    if @chat_room.save
+      flash[:success] = 'Chat room build successfully~'
+      redirect_to chat_rooms_path
+    else
+      flash[:erroe] = 'Some error(s) happened!'
+      render 'new'
+    end
+  end
+
+  private
+
+  def chat_room_params
+    params.require(:chat_room).permit(:title)
+  end
+end
