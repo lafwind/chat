b27dc31a896bb2832d43162c7772eebb5cb633dd
Lafwind <lafwind@gmail.com>
2016-06-23 14:00:10 +0800

    完成 rooms index 页面的设计

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,4 +1,13 @@
 class ChatRoom < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
+
+
+  def created_time
+    created_at.strftime('%H:%M:%S %d %B %Y')
+  end
+
+  def owner
+    User.find(user_id)
+  end
 end
