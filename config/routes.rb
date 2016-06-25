edae3ccf8c0bebb308dbd47bc19f14dd4e62313f
Lafwind <lafwind@gmail.com>
2016-06-25 21:45:16 +0800

    新增 user 的个人 profile 页面, 完成 porfile 页面和 account edit 页面的设计

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   resources :chat_rooms, only: [:index, :show, :new, :create]
+
+  get 'profile', to: 'users#show'
 
   root to: 'chat_rooms#index'
 
