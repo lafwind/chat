5c4d7410409dc48352dac0437c38b81814a49f6f
Lafwind <lafwind@gmail.com>
2016-07-19 18:23:06 +0800

    Dockerfile: 修改部署时配置

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-unless User.find_by(role: "Admin")
+if (user = User.find_by(email: 'lafwind@gmail.com')) && (!user.has_role?("Admin"))
+  user.update(role: "Admin")
+end
+
+unless (User.find_by(role: "Admin") || User.find_by(email: 'lafwind@gmail.com'))
   User.create!(name: 'lafwind', email: 'lafwind@gmail.com', password: '123456', password_confirmation: '123456', role: 'Admin')# .skip_confirmation!
 end
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 rails db:migrate
+rails db:seed
+
 puma -e production -C config/puma.rb
 # web: bundle exec puma -C config/puma.rb
