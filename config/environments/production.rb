30c15772dc211391b529df0f40af4e79db9f21a0
Lafwind <lafwind@gmail.com>
2016-07-19 21:52:32 +0800

    修改 form 样式

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.action_cable.url = "wss://chat.daoapp.io/cable"
-  config.action_cable.allowed_request_origins = ['http://chat.daoapp.io']
+  config.action_cable.url = "wss://join.daoapp.io/cable"
+  config.action_cable.allowed_request_origins = ['http://join.daoapp.io']
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://chat.daoapp.io' }
+  config.action_mailer.default_url_options = { host: 'http://join.daoapp.io' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
