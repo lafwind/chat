498722fb64fa2da233396a09f2934f6b09062e87
Lafwind <lafwind@gmail.com>
2016-07-19 20:06:07 +0800

    fix mail-sending bug

--- config/mail.rb
+++ config/mail.rb
@@ -1,13 +1,13 @@
-if Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
-    port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: :login
-  }
+# if Rails.env.production?
+#   ActionMailer::Base.smtp_settings = {
+#     address: ENV['SMTP_ADDRESS'],
+#     port: ENV['SMTP_PORT'],
+#     domain: ENV['SMTP_DOMAIN'],
+#     user_name: ENV['SMTP_USER_NAME'],
+#     password: ENV['SMTP_PASSWORD'],
+#     authentication: :login
+#   }
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.default charset: 'utf-8'
-end
+#   ActionMailer::Base.delivery_method = :smtp
+#   ActionMailer::Base.default charset: 'utf-8'
+# end
