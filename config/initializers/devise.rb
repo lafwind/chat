69706b921131a203856096cada9625b9c34c4ddc
Lafwind <lafwind@gmail.com>
2016-07-24 22:03:31 +0800

    使用 mailgun 做为邮件服务, 更改发件名

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Join <lafwind@163.com>'
+  config.mailer_sender = 'Join <no-reply@join.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
